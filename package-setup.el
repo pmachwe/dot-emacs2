cdca8a48767ef6e94ef945b3afc3c9d68b63beaf
Parikshit Machwe <pmachwe@synopsys.com>
2020-09-19 14:38:56 +0530

    Setting up package.el.

---
+++ package-setup.el
@@ -0,0 +1,21 @@
+;;; Settings for package.el pkg manager
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
+(unless package--initialized (package-initialize))
+
+;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+
+(provide 'package-setup)
