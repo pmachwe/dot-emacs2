336118db025706aa691f9cf26a08f7b122aa5db9
Parikshit Machwe <pmachwe@gmail.com>
2021-07-08 23:32:23 +0530

    Nano is overriding C-c r, so unsetting it.

--- lisp/nano-setup.el
+++ lisp/nano-setup.el
@@ -3,6 +3,7 @@
 (use-package nano
   :straight (nano-emacs :type git :host github :repo "rougier/nano-emacs")
   :init
+  ;; font etc need to be set before loading
   (setq nano-font-size 12)
   :config
   (menu-bar-mode -1)
@@ -10,6 +11,7 @@
   (scroll-bar-mode -1)
   (tooltip-mode -1)
   ;; nano override C-c r which I used for roam
+  (global-unset-key (kbd "C-c r"))
   (global-set-key (kbd "C-c C-f") 'recentf-open-files))
 
 (provide 'nano-setup)
