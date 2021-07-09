19d5db9cf021e5e73e17970eed97292611245a8c
Parikshit Machwe <pmachwe@gmail.com>
2021-07-10 00:18:55 +0530

    org-download does not work on Win10, so a simple utility to copy the latest screenshot and insert into org-file.

--- lisp/org-roam-setup.el
+++ lisp/org-roam-setup.el
@@ -1,16 +1,11 @@
 ;;; Settings for org-roam and other note taking packages
 
 (use-package org-roam
-  :bind (("C-c r f" . org-roam-find-file)
-         ("C-c r i" . org-roam-insert)
-         ("C-c r t" . org-roam-dailies-find-today)
-         ("C-c r y" . org-roam-dailies-find-yesterday)
-         ("C-c r T" . org-roam-dailies-capture-today)
-         ("C-c r r" . org-roam))
   :config
   (setq org-roam-directory "~/org-roam")
   (setq org-return-follows-link t)
   (setq org-roam-agenda-dirs (list org-roam-directory (f-join org-roam-directory "dailies")))
+  (setq org-startup-with-inline-images t)
   (if (boundp 'org-agenda-files)
       (setq org-agenda-files (append org-agenda-files org-roam-agenda-dirs))
     (setq org-agenda-files org-roam-agenda-dirs))
@@ -18,8 +13,38 @@
                                               "%?"
                                               :immediate-finish t
                                               :file-name "dailies/daily-%<%B-%m-%Y>"
-                                              :head "#+title: %<%B %m, %Y>\n\n* Tasks\n\n* Discussions"))))
+                                              :head "#+title: %<%B %m, %Y>\n\n* Tasks\n\n* Discussions")))
 
+  ;; Some functions to work on Windows
+  (defcustom device-screenshot-dir "~/OneDrive/Pictures"
+    "Directory where screenshots are to be found.")
+
+  (defun is-image-file (file)
+    (or (equal (f-ext file) "png")
+        (equal (f-ext file) "jpg")))
+
+  (defun latest-file (path)
+    "Get latest file in PATH."
+    (let (files)
+      ;; (setq files (directory-files path 'full pat t))
+      (setq files (f-files path 'is-image-file))
+      (car (sort files #'file-newer-than-file-p))))
+
+  (defun org-roam-insert-latest-screenshot()
+    (interactive)
+    (let* ((orig-file (latest-file device-screenshot-dir))
+           (file-name (f-filename orig-file))
+           (new-file-name (f-join org-roam-directory "images" file-name)))
+      (f-copy orig-file new-file-name)
+      (insert (concat "[[./" (f-relative new-file-name) "]]"))))
+
+  :bind (("C-c r f" . org-roam-find-file)
+         ("C-c r i" . org-roam-insert)
+         ("C-c r t" . org-roam-dailies-find-today)
+         ("C-c r y" . org-roam-dailies-find-yesterday)
+         ("C-c r T" . org-roam-dailies-capture-today)
+         ("C-c r r" . org-roam)
+         ("C-c r y" . org-roam-insert-latest-screenshot)))
 
 (use-package deft
   :after org-roam
@@ -35,7 +60,10 @@
   ;;key to launch deft
   (global-set-key (kbd "C-c r d") 'deft))
 
+;; Disabling as does not seem to work on Win10
+;; Evaluate and enable later.
 (use-package org-download
+  :disabled
   :config
   (setq-default org-download-method 'directory)
   (setq-default org-download-image-dir "~/org-roam/images")
