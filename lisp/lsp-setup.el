661f895aef475117fa483cafc0a163aafc9a9629
pmachwe <pmachwe@gmail.com>
2022-03-09 20:56:08 +0530

    Enable LSP for C/C++.

--- lisp/lsp-setup.el
+++ lisp/lsp-setup.el
@@ -5,6 +5,8 @@
   ;; ((pyls . "pip install python-language-server[all]")
   ;;  (jedi-language-server . "pip install jedi-language-server"))
   :hook ((python-mode . lsp)
+         (c-mode . lsp)
+         (c++-mode . lsp)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
@@ -21,7 +23,7 @@
   (define-key lsp-mode-map [remap xref-find-apropos] #'helm-lsp-workspace-symbol))
 
 (use-package ivy-lsp
-  ::straight (lsp-ivy :host github :repo "emacs-lsp/lsp-ivy")
+  :straight (lsp-ivy :host github :repo "emacs-lsp/lsp-ivy")
   :if (eq pm-completion-system 'ivy)
   :commands lsp-ivy-workspace-symbol
   :config
