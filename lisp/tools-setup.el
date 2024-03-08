4fc4a115b853f4dac85e739fc07ede63bb30606b
Parikshit Machwe <pmachwe@gmail.com>
2024-03-08 22:35:21 +0530

    Adding beframe.el setup for frame oriented buffer management.

--- lisp/tools-setup.el
+++ lisp/tools-setup.el
@@ -231,4 +231,32 @@
 (use-package evil
   :bind ("M-e" . evil-local-mode))
 
+;; Frame-oriented buffer workflow
+(use-package beframe
+  :config
+  (defvar consult-buffer-sources)
+  (declare-function consult--buffer-state "consult")
+
+  (with-eval-after-load 'consult
+    (defface beframe-buffer
+      '((t :inherit font-lock-string-face))
+      "Face for `consult' framed buffers.")
+
+    (defun my-beframe-buffer-names-sorted (&optional frame)
+      "Return the list of buffers from `beframe-buffer-names' sorted by visibility.
+With optional argument FRAME, return the list of buffers of FRAME."
+      (beframe-buffer-names frame :sort #'beframe-buffer-sort-visibility))
+
+    (defvar beframe-consult-source
+      `( :name     "Frame-specific buffers (current frame)"
+         :narrow   ?F
+         :category buffer
+         :face     beframe-buffer
+         :history  beframe-history
+         :items    ,#'my-beframe-buffer-names-sorted
+         :action   ,#'switch-to-buffer
+         :state    ,#'consult--buffer-state))
+
+    (add-to-list 'consult-buffer-sources 'beframe-consult-source)))
+
 (provide 'tools-setup)
